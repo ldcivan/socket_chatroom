9afe655ceca0557e4a584cb6c9c933e6bb4fc573
Yujio_Nako <69411867+ldcivan@users.noreply.github.com>
2023-07-12 16:06:52 +0800

    增加数据标头标尾，增加用户列表、临时私聊与查看历史十条消息功能
    TODO：增加文件传输

--- cilent.py
+++ cilent.py
@@ -1,5 +1,14 @@
 import socket
 import threading
+import logging
+import base64
+from PIL import Image
+from io import BytesIO
+import time
+
+
+# 配置日志记录器
+logging.basicConfig(filename='error.log', level=logging.ERROR)
 
 print(
 '''
@@ -7,8 +16,9 @@ This is a note tell you how to use the program
 0. Type in the password if needed, and you can ask admin to get the password
 1. Type in some word then press Enter to send message
 2. Send "#set_name#" + {your nickname} to change the random id to a certain nickname
-3. Send "#userlist" to get user list and their IP
-4. Send "#exit" to close the client
+3. Send "#userlist" to get user list and their IP, and send "#whisper#" + {target_username} + "#" + {your_msg}
+4. Send "#msg_history" to get 10 message history
+5. Send "#exit" to close the client
 '''
 )
 
@@ -26,15 +36,45 @@ client.connect((HOST, PORT))
 def receive_message():
     while True:
         try:
-            # 接收服务器的消息
-            message = client.recv(1024).decode('utf-8')
-            if message:
-                print(message)
-        except:
+            time.sleep(1)
+            # 接收数据直到遇到换行符
+            header = b''
+            if b'\r\n' not in header:
+                header += client.recv(1024)
+            # 解析标头，获取图片数据长度
+            content_length = int(header.split(b'|')[0])
+            datatype = header.split(b'|')[1].split(b'\r\n')[0].decode()
+            # 接收主体数据
+            data = b''
+            while b'<STREAMEND>' not in data:
+                data += client.recv(content_length)
+
+            if data:
+                # 解码头部和图片数据
+                if datatype != "text":
+                    # Base64 编码字符串
+                    base64_str = data.rstrip(b'<STREAMEND>').decode('utf-8')
+
+                    # 将 Base64 编码字符串转换为二进制数据
+                    image_data = base64.b64decode(base64_str)
+
+                    # 创建 BytesIO 对象
+                    image_stream = BytesIO(image_data)
+
+                    # 打开并保存图像
+                    image = Image.open(image_stream)
+                    image.save("image.jpg")  # 将图像保存为 JPEG 格式
+                else:
+                    message = data.decode('utf-8').rstrip('<STREAMEND>')
+                    print(message)
+        except Exception as e:
+            logging.error(str(e), 'position 0')
             # 如果出现异常，则关闭客户端连接
-            client.send('#exit'.encode('utf-8'))
-            client.close()
-            break
+            try:
+                client.send('#exit'.encode('utf-8'))
+                client.close()
+            except:
+                break
 
 # 创建一个新的线程来接收服务器的消息
 receive_thread = threading.Thread(target=receive_message)
@@ -47,18 +87,41 @@ while True:
         message = input()
 
         if message == '#exit':
-            client.send(message.encode('utf-8'))
+            header = f"{len(message.encode('utf-8'))}|text\r\n".ljust(1024)
+            client.send(header.encode('utf-8') + message.encode('utf-8') + b'<STREAMEND>')
             # 如果输入内容为#exit，则关闭客户端连接
             client.close()
             break
 
-        # 发送消息给服务器
-        client.send(message.encode('utf-8'))
-    except:
+        #elif message == '#send_img':
+        #    image_path = input('Please type in the path to img: ')
+        #    # 读取图片数据
+        #    with open(image_path, 'rb') as file:
+        #        image_data = file.read()
+        #    # 将二进制数据转换为 Base64 编码
+        #    base64_data = base64.b64encode(image_data)
+        #    base64_str = base64_data.decode('utf-8')
+        #    # 构造自定义标头
+        #    header = f"{len(image_data)}|image\r\n".ljust(1024)
+        #    # 发送标头和图片数据
+        #    client.sendall(header.encode('utf-8') + base64_str.encode('utf-8') + b'<STREAMEND>')
+        #    print(len(image_data), 'send image')
+        #    print(image_data)
+        else:
+            # 发送消息给服务器
+            header = f"{len(message.encode('utf-8'))}|text\r\n".ljust(1024)
+            client.send(header.encode('utf-8') + message.encode('utf-8') + b'<STREAMEND>')
+    except Exception as e:
+        logging.error(str(e), 'position 1')
         # 如果出现异常，则关闭客户端连接
-        client.send('#exit'.encode('utf-8'))
-        client.close()
+        try:
+            client.send('#exit'.encode('utf-8'))
+            client.close()
+        except:
+            pass
         break
 
 
 print('Client closed!')
+# 关闭日志文件
+logging.shutdown()
--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 import socket
 import threading
 import random
+import json
 import datetime
 import time
 import sys
 import logging
 
-
 # 配置日志输出到文件
 logging.basicConfig(filename='output.log', level=logging.INFO)
+logging.basicConfig(filename='output.log', level=logging.ERROR)
 
 
 # 重定向 print 输出到日志
@@ -16,37 +17,50 @@ class PrintLogger:
     def write(self, message):
         logging.info(message)
 
-sys.stdout = PrintLogger()
 
+sys.stdout = PrintLogger()
 
 # 设置服务器的IP和端口号
 HOST = '0.0.0.0'
 PORT = 14514
 clients = {}  # 存储客户端信息的字典，键为客户端编号，值为客户端套接字对象
 tmp_clients = {}  # 储存正在输入密码的客户端信息
+timeout = 5  # socket连接超时时间
 
 
 def get_time():
     curr_time = datetime.datetime.now()
     timestamp = datetime.datetime.strftime(curr_time, '%Y-%m-%d %H:%M:%S')
     return timestamp
- 
+
 
 def handle_client(conn, addr):
     while True:
-        client_id = random.randint(1000, 9999) # 为客户端分配一个未使用的随机编号
+        client_id = str(random.randint(1000, 9999))  # 为客户端分配一个未使用的随机编号
         if client_id not in clients and client_id not in tmp_clients:
             break
-    tmp_clients[client_id] = [conn, addr] # 将客户端信息存储到字典中
+    tmp_clients[client_id] = [conn, addr]  # 将客户端信息存储到字典中
     print(f'{get_time()} {addr} is trying connect to server')
     pw_broadcast(f'*** Please type in the password of this chatroom ***', client_id)
     while True:  # 接受密码
         time.sleep(2)
         try:
-            data = conn.recv(1024)
+            # 接收数据直到遇到换行符
+            header = b''
+            if b'\r\n' not in header:
+                header += conn.recv(1024)
+                if header == b'':
+                    continue
+            # 解析标头，获取图片数据长度
+            content_length = int(header.split(b'|')[0])
+            datatype = header.split(b'|')[1].split(b'\r\n')[0].decode()
+            # 接收主体数据
+            data = b''
+            if b'<STREAMEND>' not in data:
+                data += conn.recv(content_length)
             if not data:
                 continue
-            password = data.decode()
+            password = data.decode().rstrip('<STREAMEND>')
             if password == "Ldc123456":  # 设置密码
                 clients[client_id] = tmp_clients[client_id]
                 del tmp_clients[client_id]
@@ -54,75 +68,182 @@ def handle_client(conn, addr):
             else:
                 pw_broadcast(f'*** Wrong password ***', client_id)
                 continue
-        except:
+        except Exception as e:
+            # 记录错误日志
+            logging.error(str(e), 'position 0')
             continue
-    
+
     print(f'{get_time()} New connection from {addr}, assigned client ID: {client_id}')
     broadcast(f'\n*** User {client_id} has joined in ***\n')
     while True:  # 接受用户发文
         time.sleep(1)
         try:
-            data = conn.recv(1024)
+            # 接收数据直到遇到换行符
+            header = b''
+            if b'\r\n' not in header:
+                header += conn.recv(1024)
+                if header == b'':
+                    continue
+            # 解析标头，获取图片数据长度
+            content_length = int(header.split(b'|')[0])
+            datatype = header.split(b'|')[1].split(b'\r\n')[0].decode()
+            # 接收主体数据
+            data = b''
+            while b'<STREAMEND>' not in data:
+                data += conn.recv(content_length)
+
             if not data:
                 break
-            string = data.decode()
-            if string == "#exit":
-                break
-            if string.startswith("#set_name#"):
-                new_name = string[len("#set_name#"):]
-                print(f'{get_time()} Change name: {client_id} -> {new_name}')
-                if new_name in clients:  # 防止重复昵称
-                    single_broadcast(f'*** {new_name} has been used! ***', client_id)
-                    print(f'{get_time()} Name change failed: repeat name')
-                else:
-                    del clients[client_id]
-                    client_id = new_name
-                    clients[client_id] = [conn, addr] # 将客户端信息存储到字典中
-                    single_broadcast(f'*** You have set your name: {client_id} ***', client_id)
-                    print(f'{get_time()} Name changed: {new_name}')
-            elif string == '#userlist':
-                single_broadcast(userlist(), client_id)
+            if datatype != "text":
+                broadcast(data.rstrip(b'<STREAMEND>'))
+                print(f'User {client_id} send an image({content_length}) ({get_time()})')
             else:
-                message = f'User {client_id}: {data.decode()} ({get_time()})'
-                broadcast(message)
-                print(message)
-        except:
-            break
+                string = data.decode().rstrip('<STREAMEND>')
+                if string == "#exit":
+                    break
+                if string.startswith("#set_name#"):
+                    new_name = string[len("#set_name#"):]
+                    print(f'{get_time()} Change name: {client_id} -> {new_name}')
+                    if new_name in clients:  # 防止重复昵称
+                        single_broadcast(f'*** {new_name} has been used! ***', client_id)
+                        print(f'{get_time()} Name change failed: repeat name')
+                    else:
+                        del clients[client_id]
+                        client_id = new_name
+                        clients[client_id] = [conn, addr]  # 将客户端信息存储到字典中
+                        single_broadcast(f'*** You have set your name: {client_id} ***', client_id)
+                        print(f'{get_time()} Name changed: {new_name}')
+                elif string == '#userlist':
+                    single_broadcast(userlist(client_id), client_id)
+                elif string == '#msg_history':
+                    single_broadcast(msg_history(), client_id)
+                elif string.startswith("#whisper#"):
+                    target_client_id, message = split_whisper(string)
+                    if target_client_id in clients:
+                        single_broadcast(f'You whisper to user {target_client_id}: {message} ({get_time()})', client_id)
+                        single_broadcast(f'User {client_id} whisper to you: {message} ({get_time()})', target_client_id)
+                        print(f'User {client_id} whisper to user {target_client_id}: {message} ({get_time()})')
+                    else:
+                        single_broadcast(f'*** User not found ***', client_id)
+                else:
+                    message = f'User {client_id}: {data.decode()} ({get_time()})'
+                    broadcast(message)
+                    print(message)
+        except Exception as e:
+            # 记录错误日志
+            logging.error(str(e), 'position 1')
+            continue
     broadcast(f'\n*** User {client_id} has left ***\n')
     print(f'{get_time()} Connection {addr}({client_id}) closed')
-    del clients[client_id] # 客户端断开连接后，从字典中删除客户端信息
+    del clients[client_id]  # 客户端断开连接后，从字典中删除客户端信息
 
 
 def broadcast(message):
-    for client_id, (conn, addr) in clients.items():
+    for client_id, (conn, addr) in list(clients.items()):
         try:
-            conn.sendall(message.encode())
-        except:
+            if isinstance(message, bytes):
+                # 构造自定义标头
+                header = f"{len(message)}|image\r\n".ljust(1024)
+                # 发送标头和图片数据
+                conn.sendall(header.encode('utf-8') + message + b'<STREAMEND>')
+            else:
+                # 构造自定义标头
+                header = f"{len(message)}|text\r\n".ljust(1024)
+                # 发送标头和图片数据
+                conn.sendall(header.encode('utf-8') + message.encode('utf-8') + b'<STREAMEND>')
+        except Exception as e:
+            # 记录错误日志
+            logging.error(str(e), 'position 2')
             del clients[client_id]
-            
-           
+    if not isinstance(message, bytes):
+        msg_recorder(message)
+
+
 def single_broadcast(message, client_id):
     try:
         conn = clients[client_id][0]
-        conn.sendall(message.encode())
-    except:
+        if isinstance(message, bytes):
+            # 构造自定义标头
+            header = f"{len(message)}|image\r\n".ljust(1024)
+            # 发送标头和图片数据
+            conn.sendall(header.encode('utf-8') + message)
+        else:
+            # 构造自定义标头
+            header = f"{len(message)}|text\r\n".ljust(1024)
+            # 发送标头和图片数据
+            conn.sendall(header.encode('utf-8') + message.encode('utf-8') + b'<STREAMEND>')
+    except Exception as e:
+        # 记录错误日志
+        logging.error(str(e), 'position 3')
         pass
-    
-    
+
+
 def pw_broadcast(message, client_id):
     try:
         conn = tmp_clients[client_id][0]
-        conn.sendall(message.encode())
-    except:
+        # 构造自定义标头
+        header = f"{len(message)}|text\r\n".ljust(1024)
+        # 发送标头和图片数据
+        conn.sendall(header.encode('utf-8') + message.encode('utf-8') + b'<STREAMEND>')
+    except Exception as e:
+        # 记录错误日志
+        logging.error(str(e), 'position 4')
         pass
 
 
-def userlist():
+def userlist(req_client):
     output = '*** User List ***\n'
-    for client_id, (conn, addr) in clients.items():
-        line = f"Client ID: {client_id}, Address: {addr}\n"  # 构建每行的内容
+    for client_id, (conn, addr) in list(clients.items()):
+        if req_client != client_id:
+            line = f"Client ID: {client_id}, Address: {addr}\n"  # 构建每行的内容
+        else:
+            line = f"Client ID: {client_id}, Address: {addr} (You)\n"  # 构建每行的内容
         output += line  # 将每行内容添加到输出字符串中
     return output
+
+
+def msg_recorder(message):
+    max_messages = 10  # 最大消息数量
+    file_path = 'messages.json'  # JSON 文件路径
+
+    # 读取现有的消息列表
+    try:
+        with open(file_path, 'r') as file:
+            messages = json.load(file)
+    except FileNotFoundError:
+        messages = []
+
+    # 添加新消息到列表末尾
+    messages.append(message)
+
+    # 如果消息数量超过最大值，删除第一条消息
+    if len(messages) > max_messages:
+        messages = messages[1:]
+
+    # 保存消息列表到 JSON 文件
+    with open(file_path, 'w') as file:
+        json.dump(messages, file)
+
+
+def msg_history():
+    output = '*** Message history start ***\n'
+    # 读取 JSON 文件
+    file_path = 'messages.json'  # JSON 文件路径
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    # 打印所有值
+    for msg in data:
+        output += f'{msg}\n'
+    output += '*** Message history end ***'
+    return output
+
+
+def split_whisper(string):
+    parts = string.split('#')
+    client_id = parts[2]
+    message = parts[3]
+    return client_id, message
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
